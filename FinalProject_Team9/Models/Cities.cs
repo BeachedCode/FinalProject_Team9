77f114536cecf18ad715f01b58ca8db75e983162
markd <dsilvamj@mail.uc.edu>
2023-11-28 13:19:21 -0500

    I have added the tables

---
+++ FinalProject_Team9/Models/Cities.cs
@@ -0,0 +1,11 @@
+﻿namespace FinalProject_Team9.Models
+{
+    public class Cities
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ FinalProject_Team9/Models/Hobbies.cs
@@ -0,0 +1,11 @@
+﻿namespace FinalProject_Team9.Models
+{
+    public class Hobbies
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Hobbies { get; set; }
+        public int age { get; set; }
+    }
+}
