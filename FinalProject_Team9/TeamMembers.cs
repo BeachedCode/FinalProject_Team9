f460db97e0e97c12e0c956d273672dc063dc07ff
Ethan <Ethan@DESKTOP-CR4BC8P>
2023-11-28 12:37:49 -0500

    Add project files.

---
+++ FinalProject_Team9/TeamMembers.cs
@@ -0,0 +1,11 @@
+﻿namespace FinalProject_Team9
+{
+    public class TeamMembers
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string CollegeProgram { get; set; }
+        public string YearInProgram { get; set; }
+    }
+}
