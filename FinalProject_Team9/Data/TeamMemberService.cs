10f2412dd20ede761dab06dcb1dbbcfd22da56aa
Ethan White <white2e9@mail.uc.edu>
2023-11-30 20:37:54 -0500

    add teammember service

---
+++ FinalProject_Team9/Data/TeamMemberService.cs
@@ -0,0 +1,53 @@
+﻿using FinalProject_Team9.Models;
+
+namespace FinalProject_Team9.Data
+{
+    public class TeamMemberService : ITeamMember
+    {
+        TeamContext ctx;
+        
+        public TeamMemberService(TeamContext context)
+        {
+            ctx = context;
+        }
+
+        List<TeamMembers> ITeamMember.GetTeamMembers()
+        {
+            return ctx.Member.ToList();
+        }
+
+        public TeamMembers GetTeamMembersById(int id)
+        {
+            return ctx.Member.FirstOrDefault(x => x.Id == id);
+        }
+
+        public int? AddTeamMember(TeamMembers t)
+        {         
+            var members = this.GetTeamMembersById(t.Id);
+            if (members != null) 
+            {
+                return null;
+            }
+            ctx.Member.Add(t);
+            return ctx.SaveChanges();
+        }
+        
+        public int? RemoveTeamMemberById(int id)
+        {
+            var member = this.GetTeamMembersById(id);
+            if (member == null) 
+                {
+                    return null;
+                }
+            ctx.Member.Remove(member);
+            return ctx.SaveChanges();
+        }
+
+        public int? UpdateTeamMembers(TeamMembers team)
+        {
+            ctx.Member.Update(team);
+            return ctx.SaveChanges();
+        }
+    
+    }
+}
