d274d7caa1c66fc0df218a31dbddd61914a344d9
Calvin Yeboah <113629722+Calvinyeb04@users.noreply.github.com>
2023-12-01 22:18:30 -0500

    Added my info in the TeamContext

--- FinalProject_Team9/Data/TeamContext.cs
+++ FinalProject_Team9/Data/TeamContext.cs
@@ -14,6 +14,7 @@ namespace FinalProject_Team9.Data
                 new TeamMembers { FullName = "John Smith", Birthdate = new DateTime(2023, 11, 30), CollegeProgram = "IT", YearInProgram = "3rd" },
                 new TeamMembers { FullName = "Jane Smith", Birthdate = new DateTime(2003, 5, 15), CollegeProgram = "Cybersecurity", YearInProgram = "1st" }
                 new TeamMembers { FullName = "Ethan Beach", Birthdate = new DateTime(2002, 8, 6), CollegeProgram = "IT", YearInProgram = "3rd" }
+                new TeamMembers { FullName = "Calvin Yeboah", Birthdate = new DateTime(2004,3,25), CollegeProgram = "Computer Sceince", YearInProgram = "1st"}
             );
         }
     }
