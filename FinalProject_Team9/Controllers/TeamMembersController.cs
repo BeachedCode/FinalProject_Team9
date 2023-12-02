10734e4d93090cf9633491b0cc9e518335790d6d
Calvin Yeboah <113629722+Calvinyeb04@users.noreply.github.com>
2023-12-02 17:50:35 -0500

    Fixed the Error IDE0044 Make Field readonly

--- FinalProject_Team9/Controllers/TeamMembersController.cs
+++ FinalProject_Team9/Controllers/TeamMembersController.cs
@@ -5,11 +5,10 @@ namespace FinalProject_Team9.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-
     public class TeamMembersController : Controller
     {
+        private readonly ITeamMember ctx;
 
-        ITeamMember ctx;
         public TeamMembersController(ITeamMember teammembers)
         {
             ctx = teammembers;
@@ -21,45 +20,47 @@ namespace FinalProject_Team9.Controllers
             return Ok(ctx.GetTeamMembers());
         }
 
-        [HttpGet("id")]
-        public IActionResult Get(int id) //Read
+        [HttpGet("{id}")]
+        public IActionResult Get(int id) // Read
         {
             return Ok(ctx.GetTeamMembersById(id));
         }
 
         [HttpPost]
-        public IActionResult Post(TeamMembers team) //Create
+        public IActionResult Post(TeamMembers team) // Create
         {
             ctx.AddTeamMember(team);
             return Ok();
         }
 
         [HttpPut]
-        public IActionResult Put(TeamMembers team) //Update
+        public IActionResult Put(TeamMembers team) // Update
         {
             var result = ctx.UpdateTeamMembers(team);
             if (result == 0)
             {
-                return StatusCode(500, "An error occured while processing your request");                
+                return StatusCode(500, "An error occurred while processing your request");
             }
             return Ok();
         }
 
-        [HttpDelete("id")]
+        [HttpDelete("{id}")]
         [Route("api/delete")]
-        public IActionResult Delete(int id) //Delete
+        public IActionResult Delete(int id) // Delete
         {
             var team = ctx.GetTeamMembersById(id);
-            if(team == null)
+            if (team == null)
             {
                 return NotFound(id);
             }
             var result = ctx.RemoveTeamMembersById(id);
-            if(result == 0)
+            if (result == 0)
             {
-                return StatusCode(500, "An error occured while processing your request");
+                return StatusCode(500, "An error occurred while processing your request");
             }
             return Ok();
         }
     }
 }
+
+
