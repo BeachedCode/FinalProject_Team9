2c3dcb47f3c4aecc412103bbe004d145ce2f6c8e
Donald Durgan III <durgandl@mail.uc.edu>
2023-12-03 16:50:53 -0500

    Added my info

--- FinalProject_Team9/Data/TeamContext.cs
+++ FinalProject_Team9/Data/TeamContext.cs
@@ -20,7 +20,8 @@ namespace FinalProject_Team9.Data
             modelBuilder.Entity<TeamMembers>().HasData(
                 new TeamMembers { Id = 1, FullName = "Ethan Beach", Birthdate = new DateTime(2002, 8, 6), CollegeProgram = "IT", YearInProgram = "3rd" },
                 new TeamMembers { Id = 2, FullName = "Calvin Yeboah", Birthdate = new DateTime(2004,3,25), CollegeProgram = "Computer Science", YearInProgram = "1st"},
-                new TeamMembers { Id = 3, FullName = "Ethan White", Birthdate = new DateTime(2003, 5, 21), CollegeProgram = "IT", YearInProgram = "3rd" }
+                new TeamMembers { Id = 3, FullName = "Ethan White", Birthdate = new DateTime(2003, 5, 21), CollegeProgram = "IT", YearInProgram = "3rd" },
+                new TeamMembers { Id = 4, FullName = "Donald Durgan", Birthdate=new DateTime(2003, 9, 21), CollegeProgram = "IT", YearInProgram = "2nd" }
             );
 
 
